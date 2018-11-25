958ed2c87371ce1d8423234daa639bf3a57ec897
Sachin Filinto <sfilinto@yahoo.com>
2018-11-25 17:47:57 +0800

    Update

---
+++ 4_Manage_VMs.ps1
@@ -0,0 +1,33 @@
+﻿$RGname = "CONTOSO"
+
+# Get Status of VMs
+Get-AzureRmVM -ResourceGroupName $RGname -Status |sort-object PowerState
+
+# Start VM
+Start-AzureRmVM -ResourceGroupName $RGname -Name dc01
+
+# Stop VM
+Stop-AzureRmVM -ResourceGroupName $RGname -Name dc01 -Force
+
+# Re-Size VM ###########################################################################################################
+$RGname = "CONTOSO"
+$VMname = "server01"
+$Sizename = "Basic_A2" # Basic_A0 Basic_A1 Basic_A2
+
+$vm = Get-AzureRmVM -ResourceGroupName $RGname -VMName $VMname
+#$ops = Get-AzureRmVMSize -ResourceGroupName $RGname -VMName $VMname | select Name # List all sizes
+write-host ("Host Name        : " + $vm.Name) -ForegroundColor Red
+write-host ("Current Config   : " + $vm.HardwareProfile.VmSize) -ForegroundColor Red
+write-host ("Proposed Change  : " + $Sizename) -ForegroundColor Green
+#write-host ("Available Options: " ) -ForegroundColor Red
+#$ops
+
+$vm.HardwareProfile.VmSize = $Sizename
+Update-AzureRmVM -VM $vm -ResourceGroupName $RGname
+write-host ("Host Name        : " + $vm.Name) -ForegroundColor Green
+write-host ("After Change     : " + $vm.HardwareProfile.VmSize) -ForegroundColor Green
+
+# List all available VM sizes
+# Get-AzureRmVMSize -ResourceGroupName $RGname -VMName $VMname     
+
+
---
+++ 5_Delete_Infra_&_VMs.ps1
@@ -0,0 +1,22 @@
+﻿$RGname = "CONTOSO"
+
+######################### Get resources
+Find-AzureRmResource -ResourceGroupName $RGname | fl name, resourcetype
+Find-AzureRmResource -ResourceGroupName $RGname -ResourceNameContains server01 | fl name, resourcetype
+
+######################### Delete all resources in RG
+##### Delete VMs
+Get-AzureRmResource -ResourceGroupName $RGname -ResourceType Microsoft.Compute/virtualMachines | Remove-AzureRmVM -force
+##### Delete Disks
+Get-AzureRmResource -ResourceGroupName $RGname -ResourceType Microsoft.Compute/disks | Remove-AzureRmDisk -force
+##### Delete NIC
+Get-AzureRmResource -ResourceGroupName $RGname -ResourceType Microsoft.Network/networkInterfaces | Remove-AzureRmNetworkInterface -force
+##### Delete Public IP
+Get-AzureRmResource -ResourceGroupName $RGname -ResourceType Microsoft.Network/publicIPAddresses | Remove-AzureRmPublicIpAddress -force
+#### Delete NSG
+Get-AzureRmResource -ResourceGroupName $RGname -ResourceType Microsoft.Network/networkSecurityGroups | Remove-AzureRmNetworkSecurityGroup -Force
+#### Delete Virtual Networks
+Get-AzureRmResource -ResourceGroupName $RGname -ResourceType Microsoft.Network/virtualNetworks | Remove-AzureRmVirtualNetwork -Force
+
+######################### Delete Entire RG
+Get-AzureRmResourceGroup $RGname | Remove-AzureRmResourceGroup
